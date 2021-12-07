c1b3be40d4e266511d745a194628acc1dde86a5d
MykeFr <myke.fr66@gmail.com>
2021-12-07 19:32:44 +0000

    statistics

--- App/Statistics/statistics.js
+++ App/Statistics/statistics.js
@@ -1,37 +1,101 @@
 
 function statistics()
 {
-    const bar_ctx = document.getElementById('myChart').getContext('2d');
-    const myChart = new Chart(bar_ctx,
+    const area_ctx = document.getElementById('areaChart').getContext('2d');
+    const area_ctx_full = document.getElementById('areaChartFull').getContext('2d');
+
+    const areaConfig =
+    {
+        type: 'line',
+        data:
         {
-            type: 'horizontalBar',
-            data:
-            {
-                labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-                datasets: [
-                    {
-                        label: "Population (millions)",
-                        backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
-                        data: [2478, 5267, 734, 784, 433]
-                    }
-                ]
-            },
-            options:
-            {
-                legend:
+            labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050],
+            datasets: [
                 {
-                    display: false
+                    data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
+                    label: "Africa",
+                    borderColor: "#3e95cd",
+                    fill: false
                 },
-                title:
                 {
-                    display: true,
-                    text: 'Predicted world population (millions) in 2050'
+                    data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
+                    label: "Asia",
+                    borderColor: "#8e5ea2",
+                    fill: false
+                },
+                {
+                    data: [168, 170, 178, 190, 203, 276, 408, 547, 675, 734],
+                    label: "Europe",
+                    borderColor: "#3cba9f",
+                    fill: false
+                },
+                {
+                    data: [40, 20, 10, 16, 24, 38, 74, 167, 508, 784],
+                    label: "Latin America",
+                    borderColor: "#e8c3b9",
+                    fill: false
+                },
+                {
+                    data: [6, 3, 2, 2, 7, 26, 82, 172, 312, 433],
+                    label: "North America",
+                    borderColor: "#c45850",
+                    fill: false
                 }
+            ]
+        },
+        options:
+        {
+            responsive: true,
+            maintainAspectRatio:false,
+            title:
+            {
+                display: true,
+                text: 'World population per region (in millions)'
+            },
+
+        }
+    };
+
+    const areaChart = new Chart(area_ctx, areaConfig)
+    const areaChartFull = new Chart(area_ctx_full, areaConfig)
+
+    const bar_ctx = document.getElementById('barChart').getContext('2d');
+    const bar_ctx_full = document.getElementById('barChartFull').getContext('2d');
+    const bar_data =
+    {
+        type: 'horizontalBar',
+        data:
+        {
+            labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+            datasets: [
+                {
+                    label: "Population (millions)",
+                    backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+                    data: [2478, 5267, 734, 784, 433]
+                }
+            ]
+        },
+        options:
+        {
+            responsive: true,
+            maintainAspectRatio:false,
+            legend:
+            {
+                display: false
+            },
+            title:
+            {
+                display: true,
+                text: 'Predicted world population (millions) in 2050'
             }
         }
-        );
+    };
+    const barChart = new Chart(bar_ctx, bar_data)
+    const barChartFull = new Chart(bar_ctx_full, bar_data)
 
     const pie_ctx = document.getElementById('pieChart').getContext('2d');
+    const pie_ctx_full = document.getElementById('pieChartFull').getContext('2d');
+
     const DATA_COUNT = 5;
     const NUMBER_CFG =
     {
@@ -67,6 +131,7 @@ function statistics()
         options:
         {
             responsive: true,
+            maintainAspectRatio:false,
             plugins:
             {
                 legend:
@@ -82,49 +147,5 @@ function statistics()
         },
     };
     const pieChart = new Chart(pie_ctx, pieConfig)
-
-        const area_ctx = document.getElementById('areaChart').getContext('2d');
-
-    const areaConfig =
-    {
-  type: 'line',
-  data: {
-    labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
-    datasets: [{ 
-        data: [86,114,106,106,107,111,133,221,783,2478],
-        label: "Africa",
-        borderColor: "#3e95cd",
-        fill: false
-      }, { 
-        data: [282,350,411,502,635,809,947,1402,3700,5267],
-        label: "Asia",
-        borderColor: "#8e5ea2",
-        fill: false
-      }, { 
-        data: [168,170,178,190,203,276,408,547,675,734],
-        label: "Europe",
-        borderColor: "#3cba9f",
-        fill: false
-      }, { 
-        data: [40,20,10,16,24,38,74,167,508,784],
-        label: "Latin America",
-        borderColor: "#e8c3b9",
-        fill: false
-      }, { 
-        data: [6,3,2,2,7,26,82,172,312,433],
-        label: "North America",
-        borderColor: "#c45850",
-        fill: false
-      }
-    ]
-  },
-  options: {
-    maintainAspectRatio: false,
-    title: {
-      display: true,
-      text: 'World population per region (in millions)'
-    }
-  }
-};
-    const areaChart = new Chart(area_ctx, areaConfig)
+    const pieChartFull = new Chart(pie_ctx_full, pieConfig)
 }
